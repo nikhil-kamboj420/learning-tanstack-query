a0ec57f7262a9e735d18d58da5d2c9ad8c736502
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-04-15 17:53:55 +0530

    add queryClient and QueryClientProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
+import "./App.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import './App.css'
+import { MainLayout } from "./components/Layout/MainLayout";
+import { Home } from "./Pages/Home";
+import { FetchOld } from "./pages/FetchOld";
+import { FetchRQ } from "./pages/FetchRQ";
 
-const  App = () => {
+
+// Create a router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/trad",
+        element: <FetchOld />,
+      },
+      {
+        path: "/rq",
+        element: <FetchRQ />,
+      },
+      // {
+      //   path: "/rq/:id",
+      //   element: <FetchIndv />,
+      // },
+      // {
+      //   path: "/infinite",
+      //   element: <InfiniteScroll />,
+      // },
+    ],
+  },
+]);
+
+const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <>
-<h1>return tanstack react query</h1>
-    </>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
+  );
+};
 
-export default App
+export default App;
