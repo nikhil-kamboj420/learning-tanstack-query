6b2a7cf878fc38d3821f32a881d24cb34686c7e7
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-04-19 22:54:06 +0530

    fetch the indv. data

--- src/api/FetchApiData.jsx
+++ src/api/FetchApiData.jsx
@@ -4,7 +4,20 @@ const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
 });
 
+
 // Retrieves a list of posts from the API
 export const getPosts = () => {
   return api.get("/posts");
 };
+
+// to fetch the indv. data
+export const FetchIndvPost= async(id) => {
+  try{
+    const res = await api.get(`/posts/${id}`);
+   return  res.status === 200 ?  res.data :[];
+  }catch(e){
+    console.log(e);
+  }
+}
+
+
