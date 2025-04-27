3f9c842c1c02e005c3c3b8e696a3f3832d97d391
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-04-27 14:12:44 +0530

    pagination-section

--- src/pages/FetchRQ.jsx
+++ src/pages/FetchRQ.jsx
@@ -1,19 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { getPosts } from "../api/FetchApiData";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { fetchPosts } from "../api/FetchApiData";
 
 export const FetchRQ = () => {
-  const fetchPosts = async () => {
-    const res = await getPosts();
-    if (res.status === 200) {
-      return res.data;
-    }
-    throw new Error("Failed to fetch posts");
-  };
+  const [pageNumber, setPageNumber] = useState(0);
+
 
   const { data, isError, error, isLoading } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryKey: ["posts" , pageNumber],
+    queryFn: ()=>fetchPosts(pageNumber),
     // gcTime : 30000,
     // staleTime : 7000,
     // refetchInterval : 1000,
@@ -29,19 +25,31 @@ export const FetchRQ = () => {
   }
 
   return (
-    <ul className="section-accordion">
-      {data.map((curElem) => {
-        const { id, title, body } = curElem;
-        return (
-          <li key={id}>
-            <NavLink to={`/rq/${id}`} style={{ textDecoration: 'none'}}>
+    <>
+      <ul className="section-accordion">
+        {data.map((curElem) => {
+          const { id, title, body } = curElem;
+          return (
+            <li key={id}>
+              <NavLink to={`/rq/${id}`} style={{ textDecoration: "none" }}>
                 <p>{id}</p>
                 <h2>{title}</h2>
                 <p>{body}</p>
-            </NavLink>
-          </li>
-        );
-      })}
-    </ul>
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="pagination-section container">
+        <button
+          disabled={pageNumber === 0 ? true : false}
+          onClick={() => setPageNumber((prev) => prev - 3)}
+        >
+          Prev
+        </button>
+        <p>{pageNumber / 3 + 1}</p>
+        <button onClick={() => setPageNumber((prev) => prev + 3)}>Next</button>
+      </div>
+    </>
   );
 };
